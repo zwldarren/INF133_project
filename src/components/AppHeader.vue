27d259e934ca385f7e24876edae5bc84fe3bd422
Darren Zeng <zwldarren@gmail.com>
2024-12-03 09:07:25 +0000

    feat: add dynamic clock to app header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,27 @@
 <template>
-    <v-app-bar title="My Dashboard" app></v-app-bar>
+    <v-app-bar title="My Dashboard" app>
+        <v-toolbar-title>{{ currentTime }}</v-toolbar-title>
+    </v-app-bar>
 </template>
 
 <script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const currentTime = ref('');
+
+const updateTime = () => {
+    const now = new Date();
+    currentTime.value = now.toLocaleTimeString();
+};
+
+onMounted(() => {
+    updateTime();
+    const timer = setInterval(updateTime, 1000);
+    onBeforeUnmount(() => {
+        clearInterval(timer);
+    });
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
